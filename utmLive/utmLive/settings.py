bc747ab5e6e9e013bf15e4b8b5d0a4598bd1a21b
Jack Le <jackl32482005@gmail.com>
2025-08-18 01:31:04 -0400

    Version 5.04 (changed DEBUG mode)

--- utmLive/utmLive/settings.py
+++ utmLive/utmLive/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%#1o7abb$ldyo#=6#%%koi5__lv360w8m=06xat(u79d7t=-e#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
